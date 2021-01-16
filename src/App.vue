5ea67c43fdc6ec0551127b88fff0f5ded1d0e8cc
Kai <1114366961@qq.com>
2021-01-16 21:03:17 +0800

    1.1写好了header和postlist 组件

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <Header></Header>
+  <div class="main">
+    <PostList></PostList>
+  </div>
+  
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import Header from './components/Header'
+import PostList from './components/PostList'
+export default {
+  name:'App',
+  components:{
+    Header,PostList
   }
 }
+</script>
+
+<style >
+ .main{
+   width: 80%;
+   margin: 0 auto;
+ }
+ #app{
+   background-color: #e1e1e1;
+ }
 </style>
+
